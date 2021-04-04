299f4a7ea1178e1e4f613553b6eaea5395e05146
NotSimoh_ <61940997+NotSimoh@users.noreply.github.com>
2021-04-04 14:19:00 +0200

    added bricks
    now u place bricks instead of cobblestone lol

--- voxel.py
+++ voxel.py
@@ -1,28 +1,32 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
-texture = 'assets/texture/cobblestone.png'
-brick = 'assets/texture/brick.png'
+
 
 
 class Voxel(Button):
-    def __init__(self, position = (0,0,0)):
+    def __init__(self, position = (0,0,0), texture = 'assets/texture/cobblestone.png'):
         super().__init__(
         parent = scene,
         position = position,
         model = 'cube',
-        origin_y = 0.5,
+        origin_y = 1,
         texture = texture,
-        color = color.color(0,0,random.uniform(0.9,1)),
+        color = color.color(0,0,random.uniform(1,1)),
         scale = 1)
+
+
+
 
     def input(self, key):
         if self.hovered:
             if key == 'right mouse down':
-                voxel = Voxel(position = self.position + mouse.normal)
+                voxel = Voxel(position = self.position + mouse.normal, texture = 'assets/texture/brick.png')
 
             if key == 'left mouse down':
                 destroy(self)
+
+
 
 
 
@@ -33,7 +37,6 @@ app = Ursina()
 for z in range(20):
     for x in range(20):
         voxel = Voxel(position = (x,0,z))
-
 
 player = FirstPersonController()
 app.run()

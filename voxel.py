ba61616b3e2bbd33bd464cc539270f21810933bd
cdvhs <61940997+cdvhs@users.noreply.github.com>
2021-07-10 13:05:41 +0200

    Rewrite
    Added music, perlin noise and re-wrote the entire code, didn't add the building system yet in the rebuild. Added an installer for Windows that installs all the prerequisites :)

---
+++ voxel.py
@@ -0,0 +1,64 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from numpy import floor
+from perlin_noise import PerlinNoise
+
+app = Ursina()
+
+#Load Textures
+grass_block = load_texture('assets/texture/grass.png')
+glass_block = load_texture('assets/texture/glass.png')
+cobble_block = load_texture('assets/texture/cobblestone.png')
+brick_block = load_texture('assets/texture/bricks.png')
+#End Load Textures
+
+#Audio
+music = Audio('assets/audio/calm1.ogg',loop = True, autoplay = True)
+#End Audio
+
+#Config
+window.title = 'VoxelTesting'
+window.color = color.rgb(0,200,211)
+window.exit_button.visible = False
+application.development_mode = False
+window.borderless = False
+window.show_ursina_splash = True
+window.vsync = True
+#End Config
+
+#Fog
+scene.fog_color = color.rgb(255,255,255)
+scene.fog_density = 0.01
+#End Fog
+
+
+def input(key):
+    if key == 'escape':
+        quit()
+
+def update():
+    pass
+
+terrain = Entity(model=None,collider=None)
+noise = PerlinNoise(octaves=2,seed=2021)
+amp = 5
+freq = 69
+
+terrainWidth = 32
+for i in range(terrainWidth*terrainWidth):
+    voxel = Entity(model='cube',color=color.green)
+    voxel.x = floor(i/terrainWidth)
+    voxel.z = floor(i%terrainWidth)
+    voxel.y = floor((noise([voxel.x/freq,voxel.z/freq]))*amp)
+    voxel.parent = terrain
+
+
+terrain.combine()
+terrain.collider = 'mesh'
+terrain.texture = grass_block
+steve = FirstPersonController()
+steve.cursor.visible = False
+steve.x = steve.z = 10
+steve.y = 12
+
+app.run()

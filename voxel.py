214071992826312e7cca0614547ddd65a5df334d
cdvhs <61940997+cdvhs@users.noreply.github.com>
2021-08-27 14:58:53 +0200

    new music, new layer!
    yes i added a rock layer under the grass one. performance is now improved!

--- voxel.py
+++ voxel.py
@@ -14,18 +14,8 @@ brick_block = load_texture('assets/texture/bricks.png')
 
 #Audio
 music = Audio('assets/audio/calm1.ogg',loop = True, autoplay = True)
-foot = Audio ('assets/audio/footstep.ogg',loop = False, autoplay = False)
 #End Audio
 
-#Config
-window.title = 'VoxelTesting'
-window.color = color.rgb(0,200,211)
-window.exit_button.visible = False
-application.development_mode = False
-window.borderless = False
-window.show_ursina_splash = True
-window.vsync = True
-#End Config
 
 #Fog
 scene.fog_color = color.rgb(255,255,255)
@@ -37,31 +27,59 @@ def input(key):
     if key == 'escape':
         quit()
 
-
 def update():
     pass
 
-
+# World
 terrain = Entity(model=None,collider=None)
+rock = Entity(model=None,collider=None)
+chunk = Entity(model=None,collider=None)
 noise = PerlinNoise(octaves=2,seed=random.randint(0,696969))
 amp = random.randint(2,25)
 freq = 69
 
-terrainWidth = 50
+
+terrainWidth = 25
 for i in range(terrainWidth*terrainWidth):
     voxel = Entity(model='cube',color=color.green)
     voxel.x = floor(i/terrainWidth)
     voxel.z = floor(i%terrainWidth)
     voxel.y = floor((noise([voxel.x/freq,voxel.z/freq]))*amp)
     voxel.parent = terrain
+    voxel2 = Entity(model='cube')
+    voxel2.x = floor(i/terrainWidth)
+    voxel2.z = floor(i%terrainWidth)
+    voxel2.y = (voxel.y - 1)
+    voxel2.parent = rock
+# End World
 
-
+# Combine
 terrain.combine()
+rock.combine()
+chunk.combine()
+# End Combine
+
+
 terrain.collider = 'mesh'
 terrain.texture = grass_block
+rock.texture = cobble_block
 steve = FirstPersonController()
 steve.cursor.visible = False
 steve.x = steve.z = 10
 steve.y = 12
 
+#Chunk
+rock.parent = chunk
+terrain.parent = chunk
+#Chunk End
+
+#Config
+window.title = 'VoxelTesting'
+window.color = color.rgb(0,200,211)
+window.exit_button.visible = False
+application.development_mode = False
+window.borderless = False
+window.show_ursina_splash = True
+window.vsync = True
 app.run()
+#End Config

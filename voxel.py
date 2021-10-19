19a1194293db9d435fe5ee23a1790d04f9792e20
cdvhs <61940997+cdvhs@users.noreply.github.com>
2021-10-19 20:43:56 +0200

    yes

--- voxel.py
+++ voxel.py
@@ -1,9 +1,10 @@
 from ursina import *
-from ursina.prefabs.first_person_controller import FirstPersonController
 from numpy import floor
 from perlin_noise import PerlinNoise
 import random
 app = Ursina()
+
+
 
 #Load Textures
 grass_block = load_texture('assets/texture/grass.png')
@@ -16,7 +17,6 @@ brick_block = load_texture('assets/texture/bricks.png')
 music = Audio('assets/audio/calm1.ogg',loop = True, autoplay = True)
 #End Audio
 
-
 #Fog
 scene.fog_color = color.rgb(255,255,255)
 scene.fog_density = 0.01
@@ -28,58 +28,51 @@ def input(key):
         quit()
 
 def update():
+
+
     pass
 
+
 # World
+randomseed = random.randint(100000000,2147483647)
 terrain = Entity(model=None,collider=None)
-rock = Entity(model=None,collider=None)
 chunk = Entity(model=None,collider=None)
-noise = PerlinNoise(octaves=2,seed=random.randint(0,696969))
+noise = PerlinNoise(octaves=2,seed=randomseed)
 amp = random.randint(2,25)
 freq = 69
+print("seed: ", randomseed)
 
-
-terrainWidth = 25
+terrainWidth = 70
 for i in range(terrainWidth*terrainWidth):
     voxel = Entity(model='cube',color=color.green)
     voxel.x = floor(i/terrainWidth)
     voxel.z = floor(i%terrainWidth)
     voxel.y = floor((noise([voxel.x/freq,voxel.z/freq]))*amp)
     voxel.parent = terrain
-    voxel2 = Entity(model='cube')
-    voxel2.x = floor(i/terrainWidth)
-    voxel2.z = floor(i%terrainWidth)
-    voxel2.y = (voxel.y - 1)
-    voxel2.parent = rock
+
 # End World
 
 # Combine
 terrain.combine()
-rock.combine()
 chunk.combine()
 # End Combine
 
 
 terrain.collider = 'mesh'
 terrain.texture = grass_block
-rock.texture = cobble_block
-steve = FirstPersonController()
-steve.cursor.visible = False
-steve.x = steve.z = 10
-steve.y = 12
 
 #Chunk
-rock.parent = chunk
 terrain.parent = chunk
 #Chunk End
 
 #Config
-window.title = 'VoxelTesting'
+window.title = "ripoff minecraft"
 window.color = color.rgb(0,200,211)
 window.exit_button.visible = False
 application.development_mode = False
 window.borderless = False
 window.show_ursina_splash = True
 window.vsync = True
+EditorCamera()
 app.run()
 #End Config
